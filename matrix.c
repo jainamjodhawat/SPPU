eb22342f3799d1113796a84a77ecc22688cc0039
Jainam Jodhawat <jainam42022@gmail.com>
2024-10-27 20:58:12 +0530

    This is a code for my assingments
    It consists operators in C and Matrix solving

--- matrix.c
+++ matrix.c
@@ -5,7 +5,7 @@ int main() {
     //int a[3][3]={{1,2,3},{1,2,3},{1,2,3}};
     int a[3][3],i,j;
     long deta;
-    printf("Enter the numbers");
+    printf("Enter the numbers \n");
     for(i=0;i<=2;i++)
     {
         for(j=0;j<=2;j++)
@@ -13,16 +13,17 @@ int main() {
             scanf("%d", &a[i][j]);
         }
     }
-    printf("the numbers are");
+    printf("The numbers are \n");
     for(i=0;i<=2;i++)
     {
+        printf("\n");
         for(j=0;j<=2;j++)
         {
             printf("%d\t", a[i][j]);
         }
     }
     deta = a[0][0] * ((a[1][1]*a[2][2]) - (a[2][1]*a[1][2])) -a[0][1] * (a[1][0]* a[2][2] - a[2][0] * a[1][2]) + a[0][2] * (a[1][0] * a[2][1] - a[2][0] * a[1][1]);
-    printf("The determinant is %ld", deta);
+    printf("The determinant is %ld", deta);  //%ld used as int is converted to long 
     getch();
     return 0;
 }
